a4a2e912251139f332e46f6bd89c7f7e4d852220
Saulo de Sousa Joseph <ssj2@cin.ufpe.br>
2018-12-02 02:26:37 -0200

    transactions

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 const Chain = require('./chain.js');
+const Transaction = require('./transaction.js');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+
+const myKey = ec.keyFromPrivate('8f8646336273ed1bb606de00e0dedb9d184e45f8607c6e979af1ce8cacc2b89e');
+const myWalletAdress = myKey.getPublic('hex');
 
 let SauloCoin = new Chain();
 
-SauloCoin.addBlock("Oi");
-SauloCoin.addBlock("Olá");
-SauloCoin.addBlock("Viva");
+const tx1 = new Transaction(myWalletAdress, 'public key goes here', 1);
+tx1.signTransaction(myKey);
 
-console.log(JSON.stringify(SauloCoin, null, 4));
+SauloCoin.addTransaction(tx1);
+
+SauloCoin.minePedingTransactions(myWalletAdress);
+
+console.log(SauloCoin.getBalanceOfAdress(myWalletAdress));
+
+
+
 
 console.log(SauloCoin.isChainValid());
